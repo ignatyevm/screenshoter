e93c712cb0d21b183b9e7b3f00bc0e5970253b3c
polyndrom <polyndromdev@gmail.com>
2020-05-10 06:39:10 +1000

    Basic drawing

--- src/main/java/EditorWindow.java
+++ src/main/java/EditorWindow.java
@@ -1,9 +1,12 @@
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class EditorWindow extends Stage {
@@ -15,14 +18,46 @@ public class EditorWindow extends Stage {
         setOnCloseRequest(event -> {
             mainWindow.show();
         });
+        setResizable(false);
+        centerOnScreen();
 
-        Canvas canvas = new Canvas(screenshot.getWidth(), screenshot.getHeight());
+        int screenshotWidth = (int) screenshot.getWidth();
+        int screenshotHeight = (int) screenshot.getHeight();
+
+        System.out.println(screenshotWidth);
+        System.out.println(screenshotHeight);
+
+        Canvas canvas = new Canvas(screenshotWidth, screenshotHeight);
         GraphicsContext context = canvas.getGraphicsContext2D();
-        context.drawImage(screenshot, 0, 0, screenshot.getWidth(), screenshot.getHeight());
+        context.drawImage(screenshot, 0, 0, screenshotWidth, screenshotHeight);
 
-        HBox toolsBar = new HBox();
+        ColorPicker colorPicker = new ColorPicker(Color.RED);
+        context.setStroke(colorPicker.getValue());
+
+        colorPicker.setOnAction(event -> {
+            context.setStroke(colorPicker.getValue());
+        });
+
+        canvas.setOnMousePressed(event -> {
+            context.beginPath();
+            context.moveTo(event.getX(), event.getY());
+        });
+
+        canvas.setOnMouseDragged(event -> {
+            context.lineTo(event.getX(), event.getY());
+            context.stroke();
+        });
+
+        HBox toolsBar = new HBox(colorPicker);
+        toolsBar.setPrefSize(screenshotWidth, 50);
+        toolsBar.setMaxSize(screenshotWidth, 50);
+        toolsBar.setMinSize(screenshotWidth, 50);
+        toolsBar.setAlignment(Pos.CENTER);
+        toolsBar.setSpacing(15);
+
         VBox vBox = new VBox(toolsBar, canvas);
         Scene scene = new Scene(vBox);
+
         setScene(scene);
         show();
     }
