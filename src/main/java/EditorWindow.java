1e5bafa16e4d9cc2fa277e4937646cb29697f4fb
polyndrom <polyndromdev@gmail.com>
2020-05-10 07:09:54 +1000

    Slider for brush size selecting

--- src/main/java/EditorWindow.java
+++ src/main/java/EditorWindow.java
@@ -3,10 +3,12 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.StrokeLineCap;
 import javafx.stage.Stage;
 
 public class EditorWindow extends Stage {
@@ -15,24 +17,38 @@ public class EditorWindow extends Stage {
         super();
 
         setTitle("Screenshoter: Editor");
+        setResizable(false);
+        centerOnScreen();
         setOnCloseRequest(event -> {
             mainWindow.show();
         });
-        setResizable(false);
-        centerOnScreen();
 
         int screenshotWidth = (int) screenshot.getWidth();
         int screenshotHeight = (int) screenshot.getHeight();
 
-        System.out.println(screenshotWidth);
-        System.out.println(screenshotHeight);
-
         Canvas canvas = new Canvas(screenshotWidth, screenshotHeight);
         GraphicsContext context = canvas.getGraphicsContext2D();
+        context.setLineCap(StrokeLineCap.ROUND);
         context.drawImage(screenshot, 0, 0, screenshotWidth, screenshotHeight);
 
         ColorPicker colorPicker = new ColorPicker(Color.RED);
+
+        Slider lineWidthSlider = new Slider(1.0, 36.0, 5.0);
+        lineWidthSlider.setPrefWidth(300);
+        lineWidthSlider.setMaxWidth(300);
+        lineWidthSlider.setMinWidth(300);
+        lineWidthSlider.setMinorTickCount(4);
+        lineWidthSlider.setMajorTickUnit(5.0);
+        lineWidthSlider.setBlockIncrement(1.0);
+        lineWidthSlider.setShowTickMarks(true);
+        lineWidthSlider.setShowTickLabels(true);
+        lineWidthSlider.setSnapToTicks(true);
+        lineWidthSlider.valueProperty().addListener((obs) -> {
+            context.setLineWidth(lineWidthSlider.getValue());
+        });
+
         context.setStroke(colorPicker.getValue());
+        context.setLineWidth(lineWidthSlider.getValue());
 
         colorPicker.setOnAction(event -> {
             context.setStroke(colorPicker.getValue());
@@ -48,7 +64,7 @@ public class EditorWindow extends Stage {
             context.stroke();
         });
 
-        HBox toolsBar = new HBox(colorPicker);
+        HBox toolsBar = new HBox(colorPicker, lineWidthSlider);
         toolsBar.setPrefSize(screenshotWidth, 50);
         toolsBar.setMaxSize(screenshotWidth, 50);
         toolsBar.setMinSize(screenshotWidth, 50);
