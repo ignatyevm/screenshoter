1cc61aa7a59aaa03e0d159f3c2864b3613d7394a
polyndrom <polyndromdev@gmail.com>
2020-05-10 05:14:34 +1000

    Delay timer

--- src/main/java/ScreenshoterMain.java
+++ src/main/java/ScreenshoterMain.java
@@ -1,4 +1,7 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,6 +12,9 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.function.Consumer;
 
 public class ScreenshoterMain extends Application {
 
@@ -16,9 +22,9 @@ public class ScreenshoterMain extends Application {
     public static final int MAIN_WINDOW_HEIGHT = 200;
 
     public void start(Stage stage) {
-        Button takeScreenShotButton = new Button("Take screenshot!");
         CheckBox checkBox = new CheckBox("Hide this window?");
         checkBox.setAllowIndeterminate(false);
+
         Slider delaySlider = new Slider(0.0, 30.0, 0.0);
         delaySlider.setPrefWidth(MAIN_WINDOW_WIDTH - 20);
         delaySlider.setMaxWidth(MAIN_WINDOW_WIDTH - 20);
@@ -33,15 +39,60 @@ public class ScreenshoterMain extends Application {
         Label label = new Label("Screenshot delay");
         label.setFont(new Font(14));
 
+        Button takeScreenShotButton = new Button("Take screenshot!");
+
         VBox vBox = new VBox(label, delaySlider, takeScreenShotButton, checkBox);
         vBox.setAlignment(Pos.CENTER);
         vBox.setSpacing(15);
+
+        takeScreenShotButton.setOnAction(event -> {
+            vBox.getChildren().clear();
+            int delay = (int) delaySlider.getValue();
+            Label counter = new Label(Integer.toString(delay));
+            counter.setFont(new Font(30));
+            vBox.getChildren().add(counter);
+            startDelayTimer(delay, currentSecond -> counter.setText(Integer.toString(currentSecond)), () -> {
+                if (checkBox.isSelected()) {
+                    Platform.runLater(() -> {
+                        try {
+                            Thread.sleep(200);
+                        } catch (InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
+                        stage.hide();
+                        takeScreenshot();
+                    });
+                }
+            });
+        });
+
         BorderPane root = new BorderPane(vBox);
         Scene scene = new Scene(root, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
         stage.setTitle("Screenshoter");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void startDelayTimer(int delay, Consumer<Integer> onUpdate, Runnable onFinish) {
+        if (delay == 0) {
+            onFinish.run();
+            return;
+        }
+        final int[] k = { delay };
+        Timeline delayTimer = new Timeline(new KeyFrame(Duration.seconds(1), e -> {
+            k[0]--;
+            onUpdate.accept(k[0]);
+        }));
+        delayTimer.setCycleCount(delay);
+        delayTimer.setOnFinished(event -> {
+            onFinish.run();
+        });
+        delayTimer.play();
+    }
+
+    private void takeScreenshot() {
+
     }
 
     public static void main(String[] args) {
