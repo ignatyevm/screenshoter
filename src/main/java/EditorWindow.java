59d84c338d617de99de07623f3f0067a23a1cf4b
polyndrom <polyndromdev@gmail.com>
2020-05-11 16:26:11 +1000

    Screenshot cropping

--- src/main/java/EditorWindow.java
+++ src/main/java/EditorWindow.java
@@ -1,18 +1,23 @@
 import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.StrokeLineCap;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -22,6 +27,11 @@ import java.io.File;
 import java.io.IOException;
 
 public class EditorWindow extends Stage {
+
+    private int screenshotWidth;
+    private int screenshotHeight;
+    private boolean isCropping = false;
+    private int cropX1, cropY1, cropX2, cropY2;
 
     public EditorWindow(Stage mainWindow, Image screenshot) {
         super();
@@ -33,15 +43,30 @@ public class EditorWindow extends Stage {
             mainWindow.show();
         });
 
-        int screenshotWidth = (int) screenshot.getWidth();
-        int screenshotHeight = (int) screenshot.getHeight();
+        screenshotWidth = (int) screenshot.getWidth();
+        screenshotHeight = (int) screenshot.getHeight();
 
-        Canvas canvas = new Canvas(screenshotWidth, screenshotHeight);
-        GraphicsContext context = canvas.getGraphicsContext2D();
-        context.setLineCap(StrokeLineCap.ROUND);
-        context.drawImage(screenshot, 0, 0, screenshotWidth, screenshotHeight);
+        cropX1 = 0;
+        cropY1 = 0;
+        cropX2 = screenshotWidth;
+        cropY2 = screenshotHeight;
+
+        Canvas mainLayer = new Canvas(screenshotWidth, screenshotHeight);
+        GraphicsContext mainLayerContext = mainLayer.getGraphicsContext2D();
+        mainLayerContext.setLineCap(StrokeLineCap.ROUND);
+        mainLayerContext.drawImage(screenshot, 0, 0, screenshotWidth, screenshotHeight);
+
+        Canvas cropLayer = new Canvas(screenshotWidth, screenshotHeight);
+        GraphicsContext cropLayerContext = cropLayer.getGraphicsContext2D();
+        cropLayerContext.setLineWidth(1);
+        cropLayerContext.setStroke(Color.BLACK);
+
+        Pane layers = new Pane(mainLayer, cropLayer);
 
         ColorPicker colorPicker = new ColorPicker(Color.RED);
+        colorPicker.setOnAction(event -> {
+            mainLayerContext.setStroke(colorPicker.getValue());
+        });
 
         Slider lineWidthSlider = new Slider(1.0, 36.0, 5.0);
         lineWidthSlider.setPrefWidth(300);
@@ -54,39 +79,76 @@ public class EditorWindow extends Stage {
         lineWidthSlider.setShowTickLabels(true);
         lineWidthSlider.setSnapToTicks(true);
         lineWidthSlider.valueProperty().addListener((obs) -> {
-            context.setLineWidth(lineWidthSlider.getValue());
+            mainLayerContext.setLineWidth(lineWidthSlider.getValue());
         });
 
-        context.setStroke(colorPicker.getValue());
-        context.setLineWidth(lineWidthSlider.getValue());
+        mainLayerContext.setStroke(colorPicker.getValue());
+        mainLayerContext.setLineWidth(lineWidthSlider.getValue());
 
-        colorPicker.setOnAction(event -> {
-            context.setStroke(colorPicker.getValue());
+        Button cropButton = new Button();
+        ImageView cropButtonIcon = new ImageView(getClass().getClassLoader().getResource("crop.png").toString());
+        cropButtonIcon.setFitWidth(20);
+        cropButtonIcon.setFitHeight(20);
+        cropButton.setGraphic(cropButtonIcon);
+        cropButton.setOnAction(event -> {
+            isCropping = true;
+            cropLayer.toFront();
         });
 
-        canvas.setOnMousePressed(event -> {
-            context.beginPath();
-            context.moveTo(event.getX(), event.getY());
+        cropLayer.setOnMousePressed(event -> {
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+            if (isCropping) {
+                cropLayerContext.clearRect(0, 0, screenshotWidth, screenshotHeight);
+                cropX1 = x;
+                cropY1 = y;
+            } else {
+                mainLayerContext.beginPath();
+                mainLayerContext.moveTo(x, y);
+            }
         });
 
-        canvas.setOnMouseDragged(event -> {
-            context.lineTo(event.getX(), event.getY());
-            context.stroke();
+        cropLayer.setOnMouseDragged(event -> {
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+            if (isCropping) {
+                cropX2 = x;
+                cropY2 = y;
+                cropLayerContext.clearRect(0, 0, screenshotWidth, screenshotHeight);
+                drawSelection(cropLayerContext, cropX1, cropY1, cropX2, cropY2);
+            } else {
+                mainLayerContext.lineTo(x, y);
+                mainLayerContext.stroke();
+            }
         });
 
-        HBox toolsBar = new HBox(colorPicker, lineWidthSlider);
+        cropLayer.setOnMouseReleased(event -> {
+            isCropping = false;
+        });
+
+        HBox toolsBar = new HBox(cropButton, colorPicker, lineWidthSlider);
         toolsBar.setAlignment(Pos.CENTER);
         toolsBar.setSpacing(15);
 
         Button saveButton = new Button("Save");
         saveButton.setOnAction(event -> {
-            WritableImage writableImage = new WritableImage(screenshotWidth, screenshotHeight);
-            canvas.snapshot(null, writableImage);
+            validateCrop();
+            int cropX = cropX1;
+            int cropY = cropY1;
+            int cropWidth = cropX2 - cropX1;
+            int cropHeight = cropY2 - cropY1;
+
+            WritableImage writableImage = new WritableImage(cropWidth, cropHeight);
+            SnapshotParameters snapshotParameters = new SnapshotParameters();
+            snapshotParameters.setViewport(new Rectangle2D(cropX, cropY, cropWidth, cropHeight));
+            mainLayer.snapshot(snapshotParameters, writableImage);
+
             FileChooser fileChooser = new FileChooser();
             fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("PNG File", "*.png"));
-            fileChooser.setTitle("Select path to save");
+            fileChooser.setTitle("Save screenshot");
             fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
             File file = fileChooser.showSaveDialog(this);
+
             try {
                 ImageIO.write(SwingFXUtils.fromFXImage(writableImage, null), "png", file);
             } catch (IOException e) {
@@ -107,11 +169,28 @@ public class EditorWindow extends Stage {
 
         borderPane.setPadding(new Insets(0, 20, 0, 20));
 
-        VBox vBox = new VBox(borderPane, canvas);
+        VBox vBox = new VBox(borderPane, layers);
         Scene scene = new Scene(vBox);
 
         setScene(scene);
         show();
+    }
+
+    void validateCrop() {
+        if (cropX2 < cropX1) { int t = cropX1; cropX1 = cropX2; cropX2 = t; }
+        if (cropY2 < cropY1) { int t = cropY1; cropY1 = cropY2; cropY2 = t; }
+    }
+
+    void drawSelection(GraphicsContext context, int x1, int y1, int x2, int y2) {
+        if (x2 < x1) { int t = x1; x1 = x2; x2 = t; }
+        if (y2 < y1) { int t = y1; y1 = y2; y2 = t; }
+        Paint currentStroke = context.getStroke();
+        int currentLineWidth = (int) context.getLineWidth();
+        context.setStroke(Color.BLACK);
+        context.setLineWidth(1);
+        context.strokeRect(x1, y1, x2 - x1, y2 - y1);
+        context.setStroke(currentStroke);
+        context.setLineWidth(currentLineWidth);
     }
 
 }
